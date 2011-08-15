35c5b985c27cb9c06a5199579d6be35eed81c091
Radu Ioan Fericean <radu@fericean.ro>
2011-08-15 14:25:40 +0300

    added fabric

---
+++ fabfile.py
@@ -0,0 +1,32 @@
+from fabric.api import sudo, prompt, run, local, cd
+from fabric.decorators import runs_once, hosts, task
+from fabric.colors import green
+
+@task
+def ci():
+    """Commit localy using mercurial"""
+    comment = prompt('Commit comment: ', default='another commit from fabric')
+    local('hg ci -m "%s"' % comment)
+    push()
+
+@runs_once
+def push():
+    print(green('pushing...'))
+    local('hg push')
+
+@task
+@hosts('rif@avocadosoft.ro:22011')
+def deploy():
+    'Deploy the app to the target environment'
+    print(green('deploying...'))
+    push()
+    for app in ('mcsa', 'ian'):
+        with cd('/home/www-data/web2py/applications/' + app):
+            run('hg pul -uv')
+
+@task
+@hosts('rif@avocadosoft.ro:22011')
+def reload():
+    print(green('reloading...'))
+    'fires an uwsgi graceful reload'
+    sudo('service uwsgi-python reload')
