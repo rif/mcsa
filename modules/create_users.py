24d0a53c225e05fb6e3dc0be0065c4c2bd8a55bd
Radu Ioan Fericean <rif@love.ro>
2011-08-28 16:19:49 +0300

    now encripting password

--- modules/create_users.py
+++ modules/create_users.py
@@ -1,5 +1,7 @@
-u1 = auth.get_or_create_user({'first_name':'root', 'last_name':'root','email':'root@root.ro','password':'testus_cumulus'})
-u2 = auth.get_or_create_user({'first_name':'admin', 'last_name':'admin','email':'admin@admin.ro','password':'testus_cumulus'})
+#started like this: python web2py.py -S [APPLICATION] -M -R applications/mcsa/modules/create_users.py
+
+u1 = auth.get_or_create_user({'first_name':'root', 'last_name':'root','email':'root@root.ro','password':db.auth_user.password.validate('testus_cumulus')[0]})
+u2 = auth.get_or_create_user({'first_name':'admin', 'last_name':'admin','email':'admin@admin.ro','password':db.auth_user.password.validate('testus_cumulus')[0]})
 g1 = db.auth_group(role='admin') or auth.add_group('admin', 'Administrator')
 auth.add_membership(g1.id, u1.id)
 auth.add_membership(g1.id, u2.id)
