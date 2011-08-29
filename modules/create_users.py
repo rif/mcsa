25a9183de66a3df239452399c6739baeac00adb0
Radu Ioan Fericean <rif@love.ro>
2011-08-29 12:25:46 +0300

    pasword prompt

--- modules/create_users.py
+++ modules/create_users.py
@@ -1,7 +1,8 @@
 #started like this: python web2py.py -S [APPLICATION] -M -R applications/mcsa/modules/create_users.py
-
-u1 = auth.get_or_create_user({'first_name':'root', 'last_name':'root','email':'root@root.ro','password':db.auth_user.password.validate('testus_cumulus')[0]})
-u2 = auth.get_or_create_user({'first_name':'admin', 'last_name':'admin','email':'admin@admin.ro','password':db.auth_user.password.validate('testus_cumulus')[0]})
+pwd = raw_input('Password: ')
+print pwd
+u1 = auth.get_or_create_user({'first_name':'root', 'last_name':'root','email':'root@root.ro','password':db.auth_user.password.validate(pwd)[0]})
+u2 = auth.get_or_create_user({'first_name':'admin', 'last_name':'admin','email':'admin@admin.ro','password':db.auth_user.password.validate(pwd)[0]})
 g1 = db.auth_group(role='admin') or auth.add_group('admin', 'Administrator')
 auth.add_membership(g1.id, u1.id)
 auth.add_membership(g1.id, u2.id)
