527023454718d45680f7aed289f0f6885f00d22a
Radu Ioan Fericean <radu@fericean.ro>
2011-10-25 21:35:03 +0300

    nicere graphs

--- static/plugin_wiki/multiselect/apply_web2py.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){jQuery('[multiple]').multiSelect({
-	checkAllText: "{{=T('Check all')}}",
-	uncheckAllText:	"{{=T('Uncheck All')}}",
-	noneSelectedText: "{{=T('Select options')}}",
-	selectedText: "{{=T('selected')}}",
-});});
